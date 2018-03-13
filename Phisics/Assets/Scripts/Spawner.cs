64c091ea0b1d4aa3b08a04f51ecbf6058d9be643
Callum <callum.dunstone@gmail.com>
2018-03-13 16:21:47 +1100

    adding in art assets

---
+++ Phisics/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] objectsToSpawn;
+    public float spawnTIme;
+    float m_elapsedSpawnTime;
+
+
+	void Update ()
+    {
+        m_elapsedSpawnTime += Time.deltaTime;
+
+        if (m_elapsedSpawnTime > spawnTIme)
+        {
+            Instantiate(objectsToSpawn[0], transform.position, Quaternion.identity);
+            m_elapsedSpawnTime = 0;
+        }
+	}
+}
